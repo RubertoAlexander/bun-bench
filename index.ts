3193cbe9003a88cfdaf1b80a7ef14ba1ab89d302
Alexander Ruberto <a.ruberto@easygo.io>
2023-05-04 11:12:50 +1000

    check file

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ const complicatedFunc = async () => {
   console.log('bun write:', Bun.write)
 
   for (let i = 0; i < 20; i++) {
+    const file = Bun.file(`./large-file-${i}.json`);
+    console.log('file:', file)
     await Bun.write(Bun.file(`./large-file-${i}.json`), JSON.stringify(json));
   }
 
